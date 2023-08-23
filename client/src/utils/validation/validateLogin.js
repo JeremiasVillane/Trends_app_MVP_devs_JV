895dde2b83ea69aa1a6f7164b4cb582f7309819e
JeremiasVillane <vil.jeremias@gmail.com>
2023-08-23 13:26:57 -0300

    bugfix: validateLogin.js

--- client/src/utils/validation/validateLogin.js
+++ client/src/utils/validation/validateLogin.js
@@ -4,7 +4,7 @@ const validateLogin = (input) => {
   const EMAIL_REGEX =
     /^([\w\-\.]+)@((\[([0-9]{2,3}\.){3}[0-9]{2,3}\])|(([\w\-]+\.)+)([a-zA-Z]{2,4}))$/;
 
-  if (user.includes("@")) {
+  if (user && user.includes("@")) {
     if (!EMAIL_REGEX.test(user)) {
       errors.user = "Ingrese un email válido o su nombre de usuario";
     }
